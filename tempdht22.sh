273d1a345715ae10d305f1de38669d32707e6075
root <root@raspberrypi.(none)>
2016-01-19 08:11:01 +0100

    new data

--- tempdht22.sh
+++ tempdht22.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FECHA=$(date +\%Y\%m\%d\%H\%M:\%S)
+FECHA=$(date +\%Y\%m\%d \%H\%M\%S)
 COMA=","
 TEMP=$(python /home/pi/temp/tempDHT.py 22 4)
 echo "$FECHA$COMA$TEMP" >> /var/www/temp/tempdht22.log
