937d2ae8b6ea736522484ad3c60cf9ccd3b701a2
root <root@raspberrypi.(none)>
2016-01-19 08:23:02 +0100

    new data

--- tempdht22.sh
+++ tempdht22.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-FECHA=$(date +\%Y\%m\%d \%H\%M\%S)
+FECHA=$(date +\%Y\%m\%d\%H\%M\%S)
+ANYO=$(date +\%Y)
+MES=$(date +\%m)
+DIA=$(date +\%d)
+HORA=$(date +\%H)
+MINUTOS=$(date +\%M)
+SEGUNDOS=$(date +\%S)
+BARRA="/"
+ESPACIO=" "
+PUNTOS=":"
 COMA=","
 TEMP=$(python /home/pi/temp/tempDHT.py 22 4)
-echo "$FECHA$COMA$TEMP" >> /var/www/temp/tempdht22.log
+echo "$ANYO$BARRA$MES$BARRA$DIA$ESPACIO$HORA$PUNTOS$MINUTOS$PUNTOS$SEGUNDOS$COMA$TEMP" >> /var/www/temp/tempdht22.log
 echo "$TEMP" > /var/www/temp/tempdht22_actual.log
