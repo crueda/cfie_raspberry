e7b501b48068203172ebd073f963f6eb38467398
Carlos <crueda@gmail.com>
2016-01-22 12:36:05 +0100

    temphum

---
+++ leer_temphum.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+FECHA=$(date +\%Y\%m\%d\%H\%M\%S)
+ANYO=$(date +\%Y)
+MES=$(date +\%m)
+DIA=$(date +\%d)
+HORA=$(date +\%H)
+MINUTOS=$(date +\%M)
+SEGUNDOS=$(date +\%S)
+BARRA="/"
+ESPACIO=" "
+PUNTOS=":"
+COMA=","
+TEMP=$(python /home/pi/sensores/temphumDHT.py 22 4)
+echo "$ANYO$BARRA$MES$BARRA$DIA$ESPACIO$HORA$PUNTOS$MINUTOS$PUNTOS$SEGUNDOS$COMA$TEMP" >> /var/www/sensores/temperaturayhumedad.log
